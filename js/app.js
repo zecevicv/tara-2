0f5796a6d203f9e4076b41397ff543c3fb869fd1
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-05-10 11:27:51 +0200

    [ADD] - Homepage

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-/* #New Products (image compare viewer)
-  ======================================================= */
-const imageCompareEl = document.getElementById("image-compare");
-const options = {
-  startingPoint: 50,
-  smoothingAmount: 50
-}
-
-const viewer = new ImageCompare(imageCompareEl, options).mount();
-
 /* #Products
 ======================================================= */
 new Swiper(".products .swiper-container", {
